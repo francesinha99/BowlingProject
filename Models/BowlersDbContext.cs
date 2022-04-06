9581243b5fa6d4481e167257afd2c1bb187e4dd3
michaelpiscione <piscionemichael@gmail.com>
2022-04-06 13:12:44 -0600

    security

--- Models/BowlersDbContext.cs
+++ Models/BowlersDbContext.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Configuration;
+using BowlingProject.Models;
 using Microsoft.EntityFrameworkCore;
+
+
 
 namespace BowlingProject.Models
 {
     public class BowlersDbContext : DbContext
     {
+
         public BowlersDbContext(DbContextOptions<BowlersDbContext> options) : base(options)
         {
 
         }
+
+
         public DbSet<Bowler> Bowlers { get; set; }
         public DbSet<Team> Teams { get; set; }
     }
